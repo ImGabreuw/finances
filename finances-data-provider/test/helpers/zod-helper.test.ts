a428400564cb390ded51ba8d977dfefc9c3553a4
ImGabreuw <imgabreuw.dev@gmail.com>
2022-12-12 22:01:07 -0300

    test(data provider): add unity test for "zod-helper"

---
+++ finances-data-provider/test/helpers/zod-helper.test.ts
@@ -0,0 +1,25 @@
+import { mapPathToProperty } from "../../src/helpers/zod-helper";
+
+test("should map zod path to property of array", () => {
+  const zodPath = ["names", 1];
+
+  const underTest = mapPathToProperty(zodPath);
+
+  expect(underTest).toBe("names[1]");
+});
+
+test("should map zod path to nested properties", () => {
+  const zodPath = ["address", "zipCode"];
+
+  const underTest = mapPathToProperty(zodPath);
+
+  expect(underTest).toBe("address.zipCode");
+});
+
+test("should map zod path to nested properties with array", () => {
+  const zodPath = ["address", "names", 2];
+
+  const underTest = mapPathToProperty(zodPath);
+
+  expect(underTest).toBe("address.names[2]");
+});
