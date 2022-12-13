283aa0a07af59cf5ee0af5df4c23fd5aace7060f
ImGabreuw <imgabreuw.dev@gmail.com>
2022-12-12 22:05:02 -0300

    feat(data provider): add domain entity "Announcement"

---
+++ finances-data-provider/src/announcement-search-engine/domain/announcement.ts
@@ -0,0 +1,29 @@
+import { NotificationStatus } from "./enums/notification-status";
+import { validate } from "./schema/validation/announcement-validation-schema";
+
+export class Announcement {
+  private _notificationStatus: NotificationStatus;
+
+  constructor(
+    readonly assetCode: string,
+    readonly title: string,
+    readonly releaseDate: Date,
+    notificationStatus?: NotificationStatus
+  ) {
+    this._notificationStatus = notificationStatus || NotificationStatus.PENDING;
+
+    validate(this);
+  }
+
+  public get notificationStatus(): NotificationStatus {
+    return this._notificationStatus;
+  }
+
+  public notified(): void {
+    this._notificationStatus = NotificationStatus.NOTIFIED;
+  }
+
+  public error(): void {
+    this._notificationStatus = NotificationStatus.ERROR;
+  }
+}
