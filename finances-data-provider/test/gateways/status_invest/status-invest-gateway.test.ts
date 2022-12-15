c0704c86c04690403b2fb8d8928a3f7daa9f7679
ImGabreuw <imgabreuw.dev@gmail.com>
2022-12-14 21:54:03 -0300

    test(data provider): add unity tests for "StatusInvestGateway"

---
+++ finances-data-provider/test/gateways/status_invest/status-invest-gateway.test.ts
@@ -0,0 +1,23 @@
+import {
+  getAssetPageUrlInStatusInvest,
+  StatusInvestAnnouncementsGateway
+} from "../../../src/gateways/status_invest/status-invest-gateway";
+
+test.skip("should get AGRO3 page url in Status Invest", async () => {
+  const assetCode = "AGRO3";
+
+  const underTest = await getAssetPageUrlInStatusInvest(assetCode);
+
+  expect(underTest).toBe("https://statusinvest.com.br/acoes/agro3");
+}, 30_000);
+
+test("should get announcement info of AGRO3 from Status Invest", async () => {
+  const assetCode = "AGRO3";
+  const announcementGateway = new StatusInvestAnnouncementsGateway();
+
+  const underTest = await announcementGateway.getLastAnnouncement(assetCode);
+
+  console.log(underTest);
+
+  expect(underTest).not.toBeNull();
+}, 30_000);
