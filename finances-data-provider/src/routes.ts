168a5b235ccbf699917df509055d2b94d00e599b
ImGabreuw <imgabreuw.dev@gmail.com>
2022-12-15 15:32:02 -0300

    styles(data provider): apply Prettier format styles

--- finances-data-provider/src/routes.ts
+++ finances-data-provider/src/routes.ts
@@ -1,75 +1,81 @@
 import express, { Request, Response } from "express";
 import { GetLastAnnouncementUseCase } from "./announcement-search-engine/usecases/get-last-announcement-usecase";
-import { StatusInvestAnnouncementsGateway } from './gateways/status_invest/status-invest-gateway';
+import { StatusInvestAnnouncementsGateway } from "./gateways/status_invest/status-invest-gateway";
 import { PuppeteerLauncher } from "./puppeteer-launcher";
 import { RealStateFundFacade } from "./real-state-fund/facade/real-state-fund-facade";
 import { BaseError } from "./shared/errors/base-error";
-import { HttpStatus } from './shared/web/http-status';
+import { HttpStatus } from "./shared/web/http-status";
 import { StockFacade } from "./stocks/facade/stock-facade";
 
 export const routes = express.Router();
 
-routes.get("/announcements/:assetCode", async (request: Request, response: Response) => {
+routes.get(
+ "/announcements/:assetCode",
+ async (request: Request, response: Response) => {
   const { assetCode } = request.params;
 
   try {
-    const statusInvestGateway = new StatusInvestAnnouncementsGateway();
+   const statusInvestGateway = new StatusInvestAnnouncementsGateway();
 
-    const useCase = new GetLastAnnouncementUseCase(statusInvestGateway);
+   const useCase = new GetLastAnnouncementUseCase(statusInvestGateway);
 
-    const announcement = await useCase.execute({ assetCode });
+   const announcement = await useCase.execute({ assetCode });
 
-    return response.status(HttpStatus.OK).json(announcement)
+   return response.status(HttpStatus.OK).json(announcement);
   } catch (error) {
-    console.log(error);
+   console.log(error);
 
-    if (error instanceof BaseError) {
-      return response.status(error.getHttpStatusCode()).json({
-        message: error.message
-      })
-    }
+   if (error instanceof BaseError) {
+    return response.status(error.getHttpStatusCode()).json({
+     message: error.message,
+    });
+   }
 
-    return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-      message: `An expected error occurred. Please contact the support team.`
-    })
+   return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+    message: `An expected error occurred. Please contact the support team.`,
+   });
   }
-});
-
-routes.get(
-  "/real-state-fund/:assetCode",
-  async (request: Request, response: Response) => {
-    const { assetCode } = request.params;
-    const { browser, page } = await PuppeteerLauncher.launch();
-
-    try {
-      const result = await new RealStateFundFacade(page).searchAndExtractData(
-        assetCode
-      );
-
-      return response.json(result);
-    } catch ({ errorMessage }) {
-      return response.status(400).json({
-        error: errorMessage,
-      });
-    } finally {
-      await browser.close();
-    }
-  }
+ },
 );
 
-routes.get("/stock/:assetCode", async (request: Request, response: Response) => {
+routes.get(
+ "/real-state-fund/:assetCode",
+ async (request: Request, response: Response) => {
   const { assetCode } = request.params;
   const { browser, page } = await PuppeteerLauncher.launch();
 
   try {
-    const result = await new StockFacade(page).searchAndExtractData(assetCode);
+   const result = await new RealStateFundFacade(page).searchAndExtractData(
+    assetCode,
+   );
 
-    return response.json(result);
+   return response.json(result);
   } catch ({ errorMessage }) {
-    return response.status(400).json({
-      error: errorMessage,
-    });
+   return response.status(400).json({
+    error: errorMessage,
+   });
   } finally {
-    await browser.close();
+   await browser.close();
   }
-});
+ },
+);
+
+routes.get(
+ "/stock/:assetCode",
+ async (request: Request, response: Response) => {
+  const { assetCode } = request.params;
+  const { browser, page } = await PuppeteerLauncher.launch();
+
+  try {
+   const result = await new StockFacade(page).searchAndExtractData(assetCode);
+
+   return response.json(result);
+  } catch ({ errorMessage }) {
+   return response.status(400).json({
+    error: errorMessage,
+   });
+  } finally {
+   await browser.close();
+  }
+ },
+);
--- finances-data-provider/src/server.ts
+++ finances-data-provider/src/server.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+
 dotenv.config();
 
 import express from "express";
@@ -9,8 +10,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+ console.log(`listening on port ${port}`);
 });
 
 app.use(express.json());
-app.use(routes)
+app.use(routes);
