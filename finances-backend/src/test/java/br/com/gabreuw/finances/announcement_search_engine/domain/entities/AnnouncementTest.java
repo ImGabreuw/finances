4a8984988a5e083eb4d3e2ac910b7f65b2fc8f95
ImGabreuw <imgabreuw.dev@gmail.com>
2022-12-16 22:16:15 -0300

    tests(backend): fix unit tests of "Announcement"

--- finances-backend/src/test/java/br/com/gabreuw/finances/announcement_search_engine/domain/entities/AnnouncementTest.java
+++ finances-backend/src/test/java/br/com/gabreuw/finances/announcement_search_engine/domain/entities/AnnouncementTest.java
@@ -1,11 +1,13 @@
 package br.com.gabreuw.finances.announcement_search_engine.domain.entities;
 
+import br.com.gabreuw.finances.announcement_search_engine.domain.entities.enums.NotificationStatus;
 import br.com.gabreuw.finances.shared.validation.SelfValidation;
 import br.com.gabreuw.finances.utils.BlankSource;
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.NullSource;
 
 import java.time.LocalDate;
@@ -24,53 +26,75 @@ class AnnouncementTest {
                 .assetCode("AGRO3")
                 .title("Fato relevante")
                 .releaseDate(LocalDate.of(2022, 12, 10))
+                .downloadUrl("https://www.domain.com/download")
                 .build();
 
         assertThat(underTest.getNotificationStatus()).isEqualTo(PENDING);
     }
 
-    @DisplayName("Should create Announcenment with notified status")
-    @Test
-    void shouldCreateAnnouncenmentWithNotifiedStatus() {
+    @DisplayName("Should create Announcement with NotificationStatus different of PENDING")
+    @EnumSource(value = NotificationStatus.class, names = {"NOTIFIED", "ERROR"})
+    @ParameterizedTest
+    void shouldCreateAnnouncementWithNotificationStatusDifferentOfNotified(NotificationStatus notificationStatus) {
         var underTest = Announcement.builder()
                 .id(1L)
                 .assetCode("AGRO3")
                 .title("Fato relevante")
                 .releaseDate(LocalDate.of(2022, 12, 10))
-                .notificationStatus(NOTIFIED)
+                .notificationStatus(notificationStatus)
+                .downloadUrl("https://www.domain.com/download")
                 .build();
 
-        assertThat(underTest.getNotificationStatus()).isEqualTo(NOTIFIED);
+        assertThat(underTest.getNotificationStatus()).isNotEqualTo(PENDING);
     }
 
-    @DisplayName("Should create Announcement with error status")
+    @DisplayName("Should create Announcement with error status using error method")
     @Test
-    void shouldCreateAnnouncementWithErrorStatus() {
+    void shouldCreateAnnouncementWithErrorStatusUsingErrorMethod() {
         var underTest = Announcement.builder()
                 .id(1L)
                 .assetCode("AGRO3")
                 .title("Fato relevante")
                 .releaseDate(LocalDate.of(2022, 12, 10))
-                .notificationStatus(ERROR)
+                .downloadUrl("https://www.domain.com/download")
                 .build();
+
+        underTest.error();
 
         assertThat(underTest.getNotificationStatus()).isEqualTo(ERROR);
     }
 
-    @DisplayName("Should throw ValidationException when create Announcement with blank EnterpriseCode")
-    @BlankSource
-    @ParameterizedTest
-    void shouldThrowValidationExceptionWhenCreateAnnouncementWithBlankEnterpriseCode(String enterpriseCode) {
-        ThrowableAssert.ThrowingCallable underTest = () -> Announcement.builder()
+    @DisplayName("Should create Announcement with notified status using notified method")
+    @Test
+    void shouldCreateAnnouncementWithNotifiedStatusUsingNotifiedMethod() {
+        var underTest = Announcement.builder()
                 .id(1L)
-                .assetCode(enterpriseCode)
+                .assetCode("AGRO3")
                 .title("Fato relevante")
                 .releaseDate(LocalDate.of(2022, 12, 10))
+                .downloadUrl("https://www.domain.com/download")
+                .build();
+
+        underTest.notified();
+
+        assertThat(underTest.getNotificationStatus()).isEqualTo(NOTIFIED);
+    }
+
+    @DisplayName("Should throw ValidationException when create Announcement with blank AssetCode")
+    @BlankSource
+    @ParameterizedTest
+    void shouldThrowValidationExceptionWhenCreateAnnouncementWithBlankAssetCode(String assetCode) {
+        ThrowableAssert.ThrowingCallable underTest = () -> Announcement.builder()
+                .id(1L)
+                .assetCode(assetCode)
+                .title("Fato relevante")
+                .releaseDate(LocalDate.of(2022, 12, 10))
+                .downloadUrl("https://www.domain.com/download")
                 .build();
 
         assertThatThrownBy(underTest)
                 .isInstanceOf(SelfValidation.ValidationException.class)
-                .hasMessage("The class Announcement have its constraints violated. [Field[fieldName=assetCode, message=must not be blank, value=%s]]".formatted(enterpriseCode));
+                .hasMessage("The class Announcement have its constraints violated. [Field[fieldName=assetCode, message=must not be blank, value=%s]]".formatted(assetCode));
     }
 
     @DisplayName("Should throw ValidationException when create Announcement with blank Title")
@@ -82,6 +106,7 @@ class AnnouncementTest {
                 .assetCode("AGRO3")
                 .title(title)
                 .releaseDate(LocalDate.of(2022, 12, 10))
+                .downloadUrl("https://www.domain.com/download")
                 .build();
 
         assertThatThrownBy(underTest)
@@ -98,6 +123,7 @@ class AnnouncementTest {
                 .assetCode("AGRO3")
                 .title("Fato relevante")
                 .releaseDate(releaseDate)
+                .downloadUrl("https://www.domain.com/download")
                 .build();
 
         assertThatThrownBy(underTest)
